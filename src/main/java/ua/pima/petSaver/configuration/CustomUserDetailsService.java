fbb763408fbbdf0d4a8acbfb17d0b2aff679a281
Pima <pimaua87@gmail.com>
2024-09-27 08:54:33 +0300

    Added view layer & security configurations.

--- src/main/java/ua/pima/petSaver/configuration/CustomUserDetailsService.java
+++ src/main/java/ua/pima/petSaver/configuration/CustomUserDetailsService.java
@@ -1,7 +1,33 @@
 package ua.pima.petSaver.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ua.pima.petSaver.entity.UserSecurityInfo;
+import ua.pima.petSaver.repository.UserSecurityInfoRepository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserSecurityInfoRepository userSecurityInfoRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserSecurityInfo userSecurityInfo=userSecurityInfoRepository.findByUsername(username);
+        if(userSecurityInfo==null){
+            throw new UsernameNotFoundException("Username or Password not found");
+        }
+        return new CustomUserDetails(userSecurityInfo.getUsername(),userSecurityInfo.getPassword(),authorities());
+    }
+
+    public Collection<? extends GrantedAuthority> authorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
 }
--- src/main/java/ua/pima/petSaver/configuration/SecurityConfiguration.java
+++ src/main/java/ua/pima/petSaver/configuration/SecurityConfiguration.java
@@ -1,7 +1,27 @@
 package ua.pima.petSaver.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
+    @Autowired
+    @Qualifier("securityDatasource")
+    DataSource securityDataSource;
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
